5f827419aadfe2d78d85bac8fba8abcc0502bcbd
PieterCorp <pieter@email.com>
2021-04-19 14:55:24 +0200

    databinding

---
+++ TheStore/TheStore/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Text;
+using TheStore.Models;
+using TheStore.Services;
+
+namespace TheStore.ViewModels
+{
+    public class HomePageViewModel : BaseViewModel
+    {
+        private ObservableCollection<Jacket> jackets;
+        private JacketRepo jacketRepo;
+
+        public ObservableCollection<Jacket> Jackets
+        {
+            get { return jackets; }
+            set
+            {
+                jackets = value;
+                OnPropertyChanged(nameof(Jackets));
+            }
+        }
+
+        public HomePageViewModel()
+        {
+            jacketRepo = new JacketRepo();
+            RefreshJackets();
+        }
+
+        private void RefreshJackets()
+        {
+            try
+            {
+                List<Jacket> jackets = jacketRepo.GetJackets();
+                Jackets = new ObservableCollection<Jacket>(jackets);
+            }
+            catch (Exception e)
+            {
+                Debug.Write(e);
+            }
+        }
+    }
+}
