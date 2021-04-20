c8370e38a1d65296c03b772891725e7b555e8877
PieterCorp <pieter@email.com>
2021-04-20 07:45:19 +0200

    beetje opgekuist

---
+++ TheStore/TheStore/Services/DummyData.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using TheStore.Models;
+
+namespace TheStore.Services
+{
+    internal class DummyData
+    {
+        private IGenericRepo<Jacket> genericRepoJackets;
+        private IGenericRepo<Shoes> genericRepoShoes;
+
+        public DummyData()
+        {
+            genericRepoShoes = new GenericRepo<Shoes>();
+            genericRepoJackets = new GenericRepo<Jacket>();
+        }
+
+        public async void FillDb()
+        {
+            List<Shoes> shoes = GetShoes();
+            foreach (var shoe in shoes)
+            {
+                await genericRepoShoes.SaveProductAsync(shoe);
+            }
+
+            List<Jacket> jackets = GetJackets();
+            foreach (var jacket in jackets)
+            {
+                await genericRepoJackets.SaveProductAsync(jacket);
+            }
+        }
+
+        private List<Shoes> GetShoes()
+        {
+            List<Shoes> shoes = new List<Shoes>
+            {
+                new Shoes
+                {
+                    Id = 0,
+                    Name = "adidas",
+                    Price = 12.5,
+                    ImgUrl = "shoes1.png"
+                },
+
+                new Shoes
+                {
+                    Id = 0,
+                    Name = "nike",
+                    Price = 15.5,
+                    ImgUrl = "shoe2.png"
+                }
+            };
+            return shoes;
+        }
+
+        private List<Jacket> GetJackets()
+        {
+            List<Jacket> jackets = new List<Jacket>
+            {
+                new Jacket
+                {
+                    Id = 0,
+                    Name = "blazer",
+                    Description = "this is a jacket",
+                    Price = 12.55,
+                    IsInStock = true,
+                    Color = Color.Pink,
+                    ImgUrl = "blazer.jpg",
+                    JacketStyle = Jacket.Style.Blouson,
+                    JacketSize = Jacket.Size.Small,
+                    JacketMaterial = Jacket.Material.Leather,
+                },
+                new Jacket
+                {
+                    Id = 0,
+                    Name = "parka",
+                    Description = "this is a 2nd jacket",
+                    Price = 125.55,
+                    IsInStock = true,
+                    Color = Color.Pink,
+                    ImgUrl = "blazer.jpg",
+                    JacketStyle = Jacket.Style.Denim,
+                    JacketSize = Jacket.Size.Large,
+                    JacketMaterial = Jacket.Material.Nylon,
+                }
+            };
+            return jackets;
+        }
+    }
+}
---
+++ TheStore/TheStore/Services/GenericRepo.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheStore.Models;
+
+namespace TheStore.Services
+{
+    public class GenericRepo<T> : IGenericRepo<T> where T : Product
+    {
+        public async Task SaveProductAsync(T product)
+        {
+            using (var dbContext = new TheStoreContext())
+            {
+                if (product.Id == 0)
+                {
+                    product.DateCreated = DateTime.Now;
+                    await dbContext.AddAsync(product);
+                }
+                else
+                {
+                    product.DateModified = DateTime.Now;
+                    dbContext.Update(product);
+                }
+
+                await dbContext.SaveChangesAsync();
+            }
+        }
+
+        public async Task DeleteProductAsync(T product)
+        {
+            using (var dbContext = new TheStoreContext())
+            {
+                dbContext.Remove(product);
+                await dbContext.SaveChangesAsync();
+            }
+        }
+
+        public async Task<List<T>> GetAllProductsAsync()
+        {
+            using (var dbContext = new TheStoreContext())
+            {
+                return await dbContext.Set<T>().ToListAsync();
+            }
+        }
+
+        public async Task<T> FindProductByIdAsync(int id)
+        {
+            using (var dbContext = new TheStoreContext())
+            {
+                return await dbContext.FindAsync<T>(id);
+            }
+        }
+
+        public async Task<List<T>> FindProductsByNameAsync(string name)
+        {
+            var ListProducts = await GetAllProductsAsync();
+            return (List<T>)ListProducts.Where(x => x.Name == name);
+        }
+    }
+}
